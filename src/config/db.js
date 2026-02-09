c8c48749de88a2d902e0199c2143d3b9a5eb6e91
hussein <hhssing0@gmail.com>
2026-02-09 13:05:03 +0300

    init

--- src/config/db.js
+++ src/config/db.js
@@ -1,36 +1,16 @@
-// import pkg from 'pg'
-// const { Pool } = pkg
+import pkg from 'pg'
+const { Pool } = pkg
 
-// let pool
+let pool
 
-// if (!global.pgPool) {
-//   pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: { rejectUnauthorized: false },
-//   })
-//   global.pgPool = pool
-// } else {
-//   pool = global.pgPool
-// }
+if (!global.pgPool) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  })
+  global.pgPool = pool
+} else {
+  pool = global.pgPool
+}
 
-// export default pool
-
-// db.js
-import { Pool } from 'pg';
-// require('dotenv').config();
-
-// نتحقق إذا كنا نعمل على جهاز محلي (Development) أو على السيرفر (Production)
-// const isProduction = process.env.NODE_ENV === 'production';
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // نستخدم SSL فقط إذا كنا في بيئة الـ Production (مثل Vercel)
-  ssl: { rejectUnauthorized: false }
-});
-// الحل الجذري: إجبار كل اتصال جديد على استخدام UTF8
-pool.on('connect', (client) => {
-  client.query("SET client_encoding = 'UTF8';")
-    .catch(err => console.error('Error setting encoding:', err));
-});
-export default pool;
-export const query = (text, params) => pool.query(text, params)
+export default pool
