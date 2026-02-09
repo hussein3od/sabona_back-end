243e70a90836d35e4d9fbac45502a50244f607d6
hussein <hhssing0@gmail.com>
2026-02-09 15:18:03 +0300

    init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // استيراد كل Routes
 // import authRoutes from './src/routes/auth.routes.js'
-import productRoutes from './src/routes/product.routes.js'
+// import productRoutes from './src/routes/product.routes.js'
 // import orderRoutes from './src/routes/order.routes.js'
 // import messageRoutes from './src/routes/message.routes.js'
 // import adminRoutes from './src/routes/admin.routes.js'
@@ -16,13 +16,13 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
+// app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
 
 // routes
 // app.use('/api/auth', authRoutes)
-app.use('/api/products', productRoutes)
+// app.use('/api/products', productRoutes)
 // app.use('/api/orders', orderRoutes)
 // app.use('/api/messages', messageRoutes)
 // app.use('/api/admin', adminRoutes)
