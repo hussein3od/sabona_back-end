8d62c8954e76d5469e1c1a93717f97c127e8a2c0
hussein <hhssing0@gmail.com>
2026-02-09 12:21:02 +0300

    int

--- src/server.js
+++ src/server.js
@@ -1,12 +1,3 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.join(__dirname, '../.env') });
-
 import app from './app.js'
 
 const PORT = process.env.PORT || 5000
