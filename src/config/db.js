9a19b656ac2fccb630c78b2611d9c9e35d52aaa9
hussein <hhssing0@gmail.com>
2026-02-09 13:26:49 +0300

    init

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,30 @@
-import pkg from 'pg'
-const { Pool } = pkg
+import {Pool} from 'pg'
+// const { Pool } = pkg
 
-let pool
+// let pool
 
-if (!global.pgPool) {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  })
-  global.pgPool = pool
-} else {
-  pool = global.pgPool
-}
+// if (!global.pgPool) {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false },
+//   })
+//   global.pgPool = pool
+// } else {
+//   pool = global.pgPool
+// }
 
-export default pool
+// export default pool
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // نستخدم SSL فقط إذا كنا في بيئة الـ Production (مثل Vercel)
+  ssl: { rejectUnauthorized: false }
+});
+// الحل الجذري: إجبار كل اتصال جديد على استخدام UTF8
+pool.on('connect', (client) => {
+  client.query("SET client_encoding = 'UTF8';")
+    .catch(err => console.error('Error setting encoding:', err));
+});
+
+export default poll
