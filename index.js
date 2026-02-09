49376b09094223347cfeeb8c2b31fc73fa6a57e2
hussein <hhssing0@gmail.com>
2026-02-09 15:17:09 +0300

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-// import path from 'path'
+import path from 'path'
 
 // استيراد كل Routes
 // import authRoutes from './src/routes/auth.routes.js'
@@ -16,9 +16,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-// app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
 
 // routes
 // app.use('/api/auth', authRoutes)
